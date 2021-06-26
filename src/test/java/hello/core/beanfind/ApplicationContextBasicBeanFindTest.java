201384aa2010f730dc237f3fcf119fe6b5f3c4e0
jujubebat <bbwwpark@naver.com>
2021-06-26 17:30:42 +0900

    test: 스프링 빈 조회 학습 테스트 구현 완료

---
+++ src/test/java/hello/core/beanfind/ApplicationContextBasicBeanFindTest.java
@@ -0,0 +1,46 @@
+package hello.core.beanfind;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextBasicBeanFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanByName() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름 없이 타입으로만 조회")
+    void findBeanByType() {
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("구체 타입으로 조회") // 구체 클래스가 들어난다면 유연성이 떨어지는 것이다!
+    void findBeanByName2() {
+        MemberService memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회가 되지 않는다.")
+    void findBeanByNameX(){
+        Assertions.assertThrows(NoSuchBeanDefinitionException.class,
+            () -> ac.getBean("xxxxx", MemberService.class));
+    }
+
+}
