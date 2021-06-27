3e0e0ca041fe79adfd61e5457150b50ec3632723
jujubebat <bbwwpark@naver.com>
2021-06-27 15:08:19 +0900

    fix: 빈주입 관련 테스트 코드 오류 수정

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,44 +9,21 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderServiceImpl implements OrderService {
 
-    // 필드 주입
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
     @Autowired
-    private MemberRepository memberRepository;
-    @Autowired
-    private DiscountPolicy discountPolicy;
+    public OrderServiceImpl(MemberRepository memberRepository,
+        DiscountPolicy discountPolicy) {
+        System.out.println("OrderServiceImpl.OrderServiceImpl");
+        System.out.println("    memberRepository = " + memberRepository);
+        System.out.println("    discountPolicy = " + discountPolicy);
 
-    // setter 주입
-//    @Autowired
-//    public void setMemberRepository(MemberRepository memberRepository) {
-//        System.out.println("memberRepository = " + memberRepository);
-//        this.memberRepository = memberRepository;
-//    }
-//
-//    @Autowired
-//    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
-//        System.out.println("discountPolicy = " + discountPolicy);
-//        this.discountPolicy = discountPolicy;
-//    }
-
-    // 생성자 주입
-//    @Autowired
-//    public OrderServiceImpl(MemberRepository memberRepository,
-//        DiscountPolicy discountPolicy) {
-//        System.out.println("OrderServiceImpl.OrderServiceImpl");
-//        System.out.println("    memberRepository = " + memberRepository);
-//        System.out.println("    discountPolicy = " + discountPolicy);
-//
-//        this.memberRepository = memberRepository;
-//        this.discountPolicy = discountPolicy;
-//    }
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
 
-    // 메서드 주입
-//    @Autowired
-//    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-//        this.memberRepository = memberRepository;
-//        this.discountPolicy = discountPolicy;
-//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
